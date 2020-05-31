48ac4de0176831c7abd4521dccd69e6b776ac078
Omar Guerrero <guerrero9725@gmail.com>
2020-05-31 18:12:00 +0200

    fix: Gatsby env

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,24 +1,44 @@
 // Site Information
-exports.NAME = process.env.NAME || "My site";
-exports.SHORT_NAME = process.env.SHORT_NAME || "site";
-exports.TITLE = process.env.TITLE || "This is my site";
-exports.PREFIX = process.env.PREFIX || "";
+exports.NAME = process.env.NAME || process.env.GATSBY_NAME || "My site";
+exports.SHORT_NAME =
+  process.env.SHORT_NAME || process.env.GATSBY_SHORT_NAME || "site";
+exports.TITLE =
+  process.env.TITLE || process.env.GATSBY_TITLE || "This is my site";
+exports.PREFIX = process.env.PREFIX || process.env.GATSBY_PREFIX || "";
 exports.DESCRIPTION =
-  process.env.DESCRIPTION || "Create your own blog with Gatsby Advanced Blog";
-exports.AUTHOR = process.env.AUTHOR || "guerrero25";
-exports.PROFESSION = process.env.PROFESSION || "Web Developer";
-exports.EMAIL = process.env.EMAIL || "example@gmail.com";
-exports.KEYWORDS = process.env.AUTHOR || ["Gatsby", "ReactJS"].join(", ");
-exports.SITE_URL = process.env.SITE_URL || "site.com";
-exports.PROFILE = process.env.PROFILE || "profile";
-exports.THEME_COLOR = process.env.THEME_COLOR || "#663399";
-exports.BACKGROUND_COLOR = process.env.BACKGROUND_COLOR || "#663399";
+  process.env.DESCRIPTION ||
+  process.env.GATSBY_DESCRIPTION ||
+  "Create your own blog with Gatsby Advanced Blog";
+exports.AUTHOR =
+  process.env.AUTHOR || process.env.GATSBY_AUTHOR || "guerrero25";
+exports.PROFESSION =
+  process.env.PROFESSION || process.env.GATSBY_PROFESSION || "Web Developer";
+exports.EMAIL =
+  process.env.EMAIL || process.env.GATSBY_EMAIL || "example@gmail.com";
+exports.KEYWORDS =
+  process.env.AUTHOR ||
+  process.env.GATSBY_AUTHOR ||
+  ["Gatsby", "ReactJS"].join(", ");
+exports.SITE_URL =
+  process.env.SITE_URL || process.env.GATSBY_SITE_URL || "site.com";
+exports.PROFILE =
+  process.env.PROFILE || process.env.GATSBY_PROFILE || "profile";
+exports.THEME_COLOR =
+  process.env.THEME_COLOR || process.env.GATSBY_THEME_COLOR || "#663399";
+exports.BACKGROUND_COLOR =
+  process.env.BACKGROUND_COLOR ||
+  process.env.GATSBY_BACKGROUND_COLOR ||
+  "#663399";
 
 // Social
-exports.GITHUB_ID = process.env.GITHUB_ID || "guerrero25";
-exports.TWITTER_ID = process.env.GITHUB_ID || "twitterID";
-exports.FACEBOOK_ID = process.env.GITHUB_ID || "facebookID";
-exports.LINKEDIN_ID = process.env.GITHUB_ID || "linkedidID";
+exports.GITHUB_ID =
+  process.env.GITHUB_ID || process.env.GATSBY_GITHUB_ID || "guerrero25";
+exports.TWITTER_ID =
+  process.env.GITHUB_ID || process.env.GATSBY_GITHUB_ID || "twitterID";
+exports.FACEBOOK_ID =
+  process.env.GITHUB_ID || process.env.GATSBY_GITHUB_ID || "facebookID";
+exports.LINKEDIN_ID =
+  process.env.GITHUB_ID || process.env.GATSBY_GITHUB_ID || "linkedidID";
 
 // Disqus (Recommend you to use this)
 exports.DISQUS_ID = "sample";
