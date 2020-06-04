dca90318fab6c29fa15cacdccb18206cddb66198
Omar Guerrero <guerrero9725@gmail.com>
2020-06-04 23:19:46 +0200

    feat: Copy for what i do

--- src/components/About/WhatIDo/index.tsx
+++ src/components/About/WhatIDo/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { FaReact } from "react-icons/fa";
+import { FaReact, FaRocket, FaDraftingCompass, FaMobile } from "react-icons/fa";
 
 import Title from "../../UI/Title";
 import InfoBlock from "./InfoBlock";
@@ -15,19 +15,18 @@ function WhatIDo({}: WhatIDoProps) {
       <Grid>
         <InfoBlock icon={<FaReact />} name="Web Development">
           More than 2 years working with web technologies building complex
-          applications. I work mainly with ReactJS.
+          applications.
         </InfoBlock>
-        <InfoBlock icon={<FaReact />} name="Web Development">
-          More than 2 years working with web technologies building complex
-          applications. I work mainly with ReactJS.
+        <InfoBlock icon={<FaRocket />} name="Performance">
+          Building application not just with the latest technologies but with
+          the best performance.
         </InfoBlock>
-        <InfoBlock icon={<FaReact />} name="Web Development">
-          More than 2 years working with web technologies building complex
-          applications. I work mainly with ReactJS.
+        <InfoBlock icon={<FaDraftingCompass />} name="UX/UI">
+          Understand the users to build interfaces that add relevant value to
+          their experience.
         </InfoBlock>
-        <InfoBlock icon={<FaReact />} name="Web Development">
-          More than 2 years working with web technologies building complex
-          applications. I work mainly with ReactJS.
+        <InfoBlock icon={<FaMobile />} name="Mobile Development">
+          Building applications for Android and iOS.
         </InfoBlock>
       </Grid>
     </Container>
