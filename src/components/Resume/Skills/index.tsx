dbd8a5761bfbcbe71994466837d7a7e6863bfae1
Omar Guerrero <guerrero9725@gmail.com>
2020-06-07 14:21:17 +0200

    feat: Add frameworks section

--- src/components/Resume/Skills/index.tsx
+++ src/components/Resume/Skills/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import Title from "../../UI/Title";
 import Progress from "../../UI/Progress";
+import WhiteSpace from "../../UI/WhiteSpace";
 
 import { Container } from "./styled";
 
@@ -10,10 +11,12 @@ interface SkillsProps {}
 function Skills({}: SkillsProps) {
   return (
     <Container>
-      <Title size={21}>Coding Skills</Title>
-      <Progress label="React JS" progress={99} />
+      <Title size={21}>Technologies</Title>
       <Progress label="HTML/CSS" progress={80} />
       <Progress label="Javascript" progress={97} />
+      <WhiteSpace size={30} />
+      <Title size={21}>Frameworks/Libraries</Title>
+      <Progress label="React JS" progress={99} />
       <Progress label="React Native" progress={60} />
       <Progress label="Angular" progress={60} />
     </Container>
