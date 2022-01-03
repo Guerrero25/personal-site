eb0523fd4ad2ddcb30d2a9e5db9f369850e31236
Omar <guerrero9725gmail.com>
2022-01-03 17:01:59 -0500

    fix: Add name to inputs for netlify submittions

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -18,10 +18,16 @@ function Contact({}: ContactProps) {
         data-netlify-honeypot="bot-field"
       >
         <input type="hidden" name="form-name" value="contact" />
-        <Input required id="name" label="Full name" />
-        <Input required id="email" label="Email" />
-        <Input required id="subject" label="Subject" />
-        <Input required id="message" label="Message" as="textarea" />
+        <Input required id="name" name="name" label="Full name" />
+        <Input required id="email" name="email" label="Email" />
+        <Input required id="subject" name="subject" label="Subject" />
+        <Input
+          required
+          id="message"
+          name="message"
+          label="Message"
+          as="textarea"
+        />
         <Button type="submit">Send message</Button>
       </Form>
     </Container>
